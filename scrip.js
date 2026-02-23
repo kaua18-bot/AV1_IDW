48b36cfdad334b7c7e26d23cc2748a42f0807d7b
kaua <kaua>
2026-02-23 11:59:02 -0300

    atualização js 1

---
+++ scrip.js
@@ -0,0 +1,6 @@
+const tarefas = [];
+
+const form = document.querySelector("#form-tarefa");
+const input = document.querySelector("#form-tarefa");
+const lista = document.querySelector("#form-tarefa");
+const mensagemErro = document.querySelector("#mensagem-erro");
